2ff55cb95c5a2cc640eb8e4e52d7eb3db318ceb4
Eduardo Patrocinio <epatro@gmail.com>
2017-10-15 14:29:10 -0400

    Set path on build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,2 @@
 cd IBMWebSpherePortalServerV9.0
-docker build --build-arg URL=ftp://anonymous@linux -t portal:v90 . 2>&1 | tee /tmp/build.log
+docker build --build-arg URL=http://docker/wps_install -t portal:v90 . 2>&1 | tee /tmp/build.log
