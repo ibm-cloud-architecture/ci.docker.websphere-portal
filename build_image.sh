e9a09d45cce15f64401975281ef15561997650d2
Eduardo Patrocinio <epatro@gmail.com>
2017-10-16 17:37:51 -0400

    wps_launch

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,5 @@
-cd IBMWebSpherePortalServerV9.0
-docker build --build-arg URL=http://docker/wps_install -t portal:v90 . 2>&1 | tee /tmp/build.log
+#cd IBMWebSpherePortalServerV9.0
+#docker build --build-arg URL=http://docker/wps_install -t portal:v90 . 2>&1 | tee /tmp/build.log
+
+cd wps_launch
+docker build -t wps:v90 
