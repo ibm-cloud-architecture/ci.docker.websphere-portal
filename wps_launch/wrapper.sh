58746eacef0d471568d33e64974e95d4570170f7
Eduardo Patrocinio <epatro@gmail.com>
2017-10-16 21:07:40 -0400

    Specify shell in wrapper

--- wps_launch/wrapper.sh
+++ wps_launch/wrapper.sh
@@ -1,3 +1,4 @@
-/opt/IBM/WebSphere/wp_profile/bin/startServer.sh WebSphere_Portal &
+#!/bin/bash
+opt/IBM/WebSphere/wp_profile/bin/startServer.sh WebSphere_Portal &
 tail -f /opt/IBM/WebSphere/wp_profile/logs/WebSphere_Portal/SystemOut.log
 
