48b31e5f8c15010b48652b111162b2d3427206f0
Eduardo Patrocinio <epatro@gmail.com>
2017-10-16 21:16:09 -0400

    Add / to wrapper.sh

--- wps_launch/wrapper.sh
+++ wps_launch/wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-opt/IBM/WebSphere/wp_profile/bin/startServer.sh WebSphere_Portal &
+/opt/IBM/WebSphere/wp_profile/bin/startServer.sh WebSphere_Portal &
 tail -f /opt/IBM/WebSphere/wp_profile/logs/WebSphere_Portal/SystemOut.log
 
