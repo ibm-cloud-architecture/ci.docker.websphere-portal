69dddeda493937eae905dec86d5bf762072cc7b6
Eduardo Patrocinio <epatro@gmail.com>
2017-10-21 08:25:11 -0400

    Deploy as Docker container

--- wps_launch/wrapper.sh
+++ wps_launch/wrapper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 /opt/IBM/WebSphere/wp_profile/bin/startServer.sh WebSphere_Portal &
+echo Sleeping 2 minutes
+sleep 120
 tail -f /opt/IBM/WebSphere/wp_profile/logs/WebSphere_Portal/SystemOut.log
 
