9c1f2ad113520c7cc38ef82b45aed70a87c39a17
Eduardo Patrocinio <epatro@gmail.com>
2017-10-23 10:09:35 -0400

    Add nginx

--- deploy_wps.sh
+++ deploy_wps.sh
@@ -1,4 +1,7 @@
 ./create_namespace.sh wps
 
-helm delete --purge wps
-helm install --name wps ./wps
+#helm delete --purge wps
+#helm install --name wps ./wps
+
+helm delete --purge nginx
+helm install --name nginx ./wps/charts/nginx
