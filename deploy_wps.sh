97075a40d5666924d6e6af0c085f053943491bf1
Eduardo Patrocinio <epatro@gmail.com>
2017-10-23 19:15:21 -0400

    Change to NodePort

--- deploy_wps.sh
+++ deploy_wps.sh
@@ -1,7 +1,5 @@
 ./create_namespace.sh wps
 
-#helm delete --purge wps
-#helm install --name wps ./wps
+helm delete --purge wps
+helm install --name wps ./wps
 
-helm delete --purge nginx
-helm install --name nginx ./wps/charts/nginx
